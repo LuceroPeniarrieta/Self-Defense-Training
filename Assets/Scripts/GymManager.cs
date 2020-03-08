9701843c867ec9f8f88619028f9b58b62dc94c83
Lucero Peñarrieta Vergara <lucero.peniarrieta@gmail.com>
2020-03-08 14:20:40 -0400

    Menus logic added - Videos exercises added

--- Assets/Scripts/GymManager.cs
+++ Assets/Scripts/GymManager.cs
@@ -1,13 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GymManager : MonoBehaviour
 {
+    public string sceneName;
+
     public void LoadNewScene() 
     {
         SceneManager.LoadScene("gym env", LoadSceneMode.Single);
         SceneManager.LoadScene("KinectAvatarsDemo1", LoadSceneMode.Additive);
+    }
+
+    public void UnloadScene() 
+    {
+        SceneManager.UnloadSceneAsync(sceneName);
+        SceneManager.LoadScene("Dashboard", LoadSceneMode.Single);
+    }
+
+    public void ExitScene()
+    {
+        SceneManager.UnloadSceneAsync(sceneName);
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void ExitGym()
+    {
+        SceneManager.UnloadSceneAsync("gym env");
+        SceneManager.UnloadSceneAsync("KinectAvatarsDemo1");
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void LoadVideo()
+    {
+        SceneManager.UnloadSceneAsync("gym env");
+        SceneManager.UnloadSceneAsync("KinectAvatarsDemo1");
+        SceneManager.LoadScene(ScenesManager.Instance.currentScene, LoadSceneMode.Single);
+    }
+
+    public void LoadDashboard()
+    {
+        SceneManager.UnloadSceneAsync("gym env");
+        SceneManager.UnloadSceneAsync("KinectAvatarsDemo1");
+        SceneManager.LoadScene("Dashboard", LoadSceneMode.Single);
     }
 }
--- Assets/Scripts/ScenesManager.cs
+++ Assets/Scripts/ScenesManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ScenesManager : MonoBehaviour
@@ -11,34 +9,56 @@ public class ScenesManager : MonoBehaviour
     public string elbowStrike;
     public string alternativeElbowStrike;
     public string bearHugAttack;
+    public string currentScene;
+
+    private static ScenesManager _instance;
+
+    public static ScenesManager Instance { get; private set; } // static singleton
+    void Awake()
+    {
+        if (Instance == null) { Instance = this; }
+        else { Destroy(gameObject); }
+    }
 
     public void LoadHeelPalmStrikeScene() 
     {
         SceneManager.LoadScene(heelPalmStrikeScene, LoadSceneMode.Single);
+        currentScene = heelPalmStrikeScene;
     }
 
     public void LoadHammerStrikeScene()
     {
         SceneManager.LoadScene(hammerStrikeScene, LoadSceneMode.Single);
+        currentScene = hammerStrikeScene;
     }
 
     public void LoadGroinKickScene()
     {
         SceneManager.LoadScene(groinKickScene, LoadSceneMode.Single);
+        currentScene = groinKickScene;
     }
 
     public void LoadElbowStrikeScene()
     {
         SceneManager.LoadScene(elbowStrike, LoadSceneMode.Single);
+        currentScene = elbowStrike;
     }
 
     public void LoadAlternativeElbowStrikeScene()
     {
         SceneManager.LoadScene(alternativeElbowStrike, LoadSceneMode.Single);
+        currentScene = alternativeElbowStrike;
     }
 
     public void LoadBearHugAttackScene()
     {
         SceneManager.LoadScene(bearHugAttack, LoadSceneMode.Single);
+        currentScene = bearHugAttack;
+    }
+
+    public void Back()
+    {
+        SceneManager.UnloadSceneAsync("Dashboard");
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
     }
 }
